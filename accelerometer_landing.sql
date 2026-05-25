1558e0a1208846d52c3d9455e57ffda2bbfc56c5
AGIL P VARGHESE <146162525+agil-p-varghese@users.noreply.github.com>
2026-05-25 11:17:19 +0530

    Create external table for accelerometer data

---
+++ accelerometer_landing.sql
@@ -0,0 +1,9 @@
+create external table accelerometer_landing(
+    timestamp bigint, 
+    user string,
+    x double,
+    y double,
+    z double 
+)
+ROW FORMAT SERDE 'org.openx.data.jsonserde.JsonSerDe'
+LOCATION 's3://agil-final-project/landing/accelerometer/';
