1400573bb8012366dce748d36682cae37a43b953
AGIL P VARGHESE <146162525+agil-p-varghese@users.noreply.github.com>
2026-05-26 12:29:30 +0530

    re

--- step_trainer_trusted.py
+++ step_trainer_trusted.py
@@ -1,22 +1,22 @@
 from awsglue.context import GlueContext
 from pyspark.context import SparkContext
 
-# Create contexts
+
 sc = SparkContext()
 glueContext = GlueContext(sc)
 spark = glueContext.spark_session
 
-# Read step trainer landing data
+
 step_df = spark.read.json(
     "s3://agil-final-project/landing/step_trainer/"
 )
 
-# Read customer curated data
+
 customer_df = spark.read.json(
     "s3://agil-final-project/curated/customer/"
 )
 
-# Join using serialNumber
+
 step_trainer_trusted = step_df.join(
     customer_df,
     step_df["serialNumber"] == customer_df["serialNumber"],
@@ -27,7 +27,7 @@ step_trainer_trusted = step_df.join(
     step_df["distanceFromObject"]
 )
 
-# Write 
+ 
 step_trainer_trusted.write.mode("overwrite").json(
     "s3://agil-final-project/trusted/step_trainer/"
 )
