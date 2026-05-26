9932f838a02f63e807e4d909c01aae42b2e3570d
AGIL P VARGHESE <146162525+agil-p-varghese@users.noreply.github.com>
2026-05-26 12:26:45 +0530

    re

--- customer_landing_to_trusted.py
+++ customer_landing_to_trusted.py
@@ -2,22 +2,21 @@ from awsglue.context import GlueContext
 from pyspark.context import SparkContext
 from pyspark.sql.functions import col
 
-# Create contexts
+
 sc = SparkContext()
 glueContext = GlueContext(sc)
 spark = glueContext.spark_session
 
-# Read customer landing data
 customer_df = spark.read.json(
     "s3://agil-final-project/landing/customer/"
 )
 
-# Filter customers who agreed to research
+
 customer_trusted = customer_df.filter(
     col("shareWithResearchAsOfDate").isNotNull()
 )
 
-# Write trusted customer data
+
 customer_trusted.write.mode("append").json(
     "s3://agil-final-project/trusted/customer/"
 )
